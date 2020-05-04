210b6a409d078516fdd6e7325afa4d6b8a8b31b6
asurath007 <asu.rath.007@gmail.com>
2020-05-04 12:00:24 +0530

    date picker

--- app/src/main/java/com/slotbooker/Admin/Admin.java
+++ app/src/main/java/com/slotbooker/Admin/Admin.java
@@ -72,7 +72,7 @@ public class Admin extends AppCompatActivity implements AdapterView.OnItemSelect
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String item = parent.getItemAtPosition(position).toString();
-        String text = parent.getSelectedItem().toString();
+//        String text = parent.getSelectedItem().toString();
 
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
